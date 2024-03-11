598bb0b16961f7da80880781a097ad2a1cd1f11a
demolayprata <ricardofrancosouza@hotmail.com>
2024-03-11 08:34:02 -0300

    adjustment to use listView

--- app/src/main/java/br/com/dominando/android/basico/MainActivity.kt
+++ app/src/main/java/br/com/dominando/android/basico/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.Text
@@ -18,46 +21,17 @@ import br.com.dominando.android.basico.ui.theme.BasicoTheme
 import org.parceler.Parcels
 
 class MainActivity : ComponentActivity() {
+
+    var names = arrayListOf<String>()
+    var adapter: ArrayAdapter<String>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i("NGVL", "tela1::onCreate")
         setContentView(R.layout.activity_main)
-        val editText = findViewById<EditText>(R.id.editTexto)
-        val buttonClick = View.OnClickListener {
-            it.setOnClickListener {
-                val text = editText.text.toString()
-                Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
-            }
-        }
-        val buttonTelaClick = View.OnClickListener {
-            it.setOnClickListener {
-                val intent = Intent(this, Tela2Activity::class.java)
-                intent.putExtra("nome", "Glauber")
-                intent.putExtra("idade", 35)
-                startActivity(intent)
-            }
-        }
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, names)
+        val lstNames: ListView = findViewById<ListView>(R.id.lstNames)
+        lstNames.adapter = adapter
 
-        val buttonParcelClick = View.OnClickListener {
-            val cliente = Cliente(codigo = 1, nome = "Bruce")
-            val intent = Intent(this, Tela2Activity::class.java)
-            intent.putExtra("cliente", Parcels.wrap(cliente))
-            startActivity(intent)
-        }
-
-        val buttonSerializableClick = View.OnClickListener {
-            val intent = Intent(this, Tela2Activity::class.java)
-            intent.putExtra("pessoa", Pessoa(nome = "Barion", idade = 35))
-            startActivity(intent)
-        }
-        val button = findViewById<Button>(R.id.buttonToast)
-        button.setOnClickListener(buttonClick)
-        val buttonParcelable = findViewById<Button>(R.id.buttonParcel)
-        val buttonSerializable = findViewById<Button>(R.id.buttonSerializable)
-        buttonSerializable.setOnClickListener(buttonSerializableClick)
-        buttonParcelable.setOnClickListener(buttonParcelClick)
-        val buttonTela2 = findViewById<Button>(R.id.buttonTela2)
-        buttonTela2.setOnClickListener(buttonTelaClick)
     }
 
     override fun onStart(){
@@ -87,20 +61,12 @@ class MainActivity : ComponentActivity() {
         super.onDestroy()
         Log.i("NGVL","Tela1::onDestroy")
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BasicoTheme {
-        Greeting("Android")
+    fun btnAddClick(view: View) {
+        val edtName = findViewById<TextView>(R.id.edtName)
+        names.add(edtName.text.toString())
+        edtName.text = ""
+        adapter?.notifyDataSetChanged()
     }
 }
+
